e8dd89475dc1006485fd645833c3d03584ce8347
unknown <anthoizu@yahoo.com>
2023-02-28 10:44:12 -0500

    Imported new component into App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import './App.css';
 import { TVShowAPI } from './Data/Data';
+import { Searchbar } from './components/searchbar';
+
 
 
 TVShowAPI.fetchPopulars();
@@ -9,11 +11,12 @@ function App() {
   return (
     <div className="App">
       <h1>Popular TV Show Lookup</h1>
+      <Searchbar />
 
-      <form className="d-flex container" role="search">
+      {/* <form className="d-flex container" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
         <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      </form> */}
 
 
 
