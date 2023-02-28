9865ad64925264689f1d940f1e0303869efc42e5
unknown <anthoizu@yahoo.com>
2023-02-27 21:11:20 -0500

    First commit for source files

---
+++ src/App.js
@@ -0,0 +1,39 @@
+
+import './App.css';
+import { TVShowAPI } from './Data/Data';
+
+
+TVShowAPI.fetchPopulars();
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Popular TV Show Lookup</h1>
+
+      <form className="d-flex container" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+
+
+
+
+      {/* <header className="App-header">
+        
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/config.js
@@ -0,0 +1,9 @@
+const BASE_URL = "https://api.themoviedb.org/3/";
+const API_KEY_PARAM = "?api_key=752dba902a872f6001e92845f040516c";
+
+
+// const SMALL_IMA_COVER_BASE_URL = "https://www.themoviedb.org/t/p/w1280/uKvVjHNqB5VmOrdxqAt2F7J78ED.jpg";
+
+// https://www.themoviedb.org/t/p/w1280/uKvVjHNqB5VmOrdxqAt2F7J78ED.jpg
+
+export { BASE_URL, API_KEY_PARAM };
